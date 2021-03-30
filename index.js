af1cc65b16c7add7a478193138c8a9b499a35ee6
abad89 <abadalamenti89@gmail.com>
2021-03-30 04:54:37 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity=`roller-skate`) {
+    return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(job=`go to the office`) {
+    return `This Monday, I will ${job}.`
+};
+
+function wrapAdjective(flair=`*`) {
+   return  function(param1=`special`) {
+        return `You are ${flair}${param1}${flair}!`
+    }
+};
+
+const Calculator = {
+    add : function(x, y) {
+        return (x+y)
+    },
+    subtract : function(x, y) {
+        return (x-y)
+    },
+    multiply : function(x,y) {
+        return (x*y)
+    },
+    divide : function(x,y) {
+        return (x/y)
+    }
+}
+
+function actionApplyer(startingInteger, funcArray) {
+    if (funcArray.length === 0) {
+        return startingInteger
+    }
+    else {
+       return (((startingInteger*2)+1000)%7)
+    }
+};
